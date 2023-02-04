bf0d1279a72027209b49020ee653a567f8035538
Chrsteffen <Chrsteffen1@gmail.com>
2023-02-03 23:10:07 -0500

    add hobby details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path("hobbies/", views.hobbies_index, name="hobbies_index"),
+  path('hobbies/<int:hobby_id>/', views.hobbies_detail, name='hobbies_detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -14,3 +14,6 @@ def hobbies_index(request):
   hobbies = Hobby.objects.all()
   return render(request, 'hobbies/index.html', {'hobbies': hobbies})
 
+def hobbies_detail(request, hobby_id):
+  hobby = Hobby.objects.get(id=hobby_id)
+  return render(request, 'hobbies/detail.html', { 'hobby': hobby})
