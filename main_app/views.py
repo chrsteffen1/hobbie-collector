c01835ac0e7f38d9d05fc03d2b53f2f3b7594c24
Chrsteffen <Chrsteffen1@gmail.com>
2023-02-03 22:44:34 -0500

    import Hobby model

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Hobby
 
 # Create your views here.
 from django.http import HttpResponse
@@ -10,15 +11,6 @@ def about(request):
   return render(request, 'about.html')
 
 def hobbies_index(request):
+  hobbies = Hobby.objects.all()
   return render(request, 'hobbies/index.html', {'hobbies': hobbies})
 
-class Hobby:
-  def __init__(self, name, level, description, price):
-    self.name = name
-    self.level = level
-    self.description = description
-    self.price = price
-
-hobbies = [
-  Hobby('Woodworking', 'Beginner', 'building things with your hands', 6)
-]
